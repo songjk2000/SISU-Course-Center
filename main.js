57ac7f687d7f8bb99e3ea06dc51d67e5b75ca0a4
Leonel Song <45224952+songjk2000@users.noreply.github.com>
2020-03-23 23:25:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-var length = 0
-var fcfileid = 0
-var fcid = 0
-var got = false
+var length = 0;
+var fcfileid = 0;
+var fcid = 0;
+var got = false;
 
 function Toast(msg,duration){
     var m = document.createElement('div');
     var democlass=document.createAttribute("class");
     democlass.value="democlass";
-    m.setAttributeNode(democlass)
+    m.setAttributeNode(democlass);
     m.innerHTML = msg;
     m.style.cssText="width:100%;height:100%;color: white;text-align: center;position: fixed;padding-top:20%;top: 0%;left: 0%;z-index: 999999;background-color: black;font-size: 30px;font-weight:bold;";
     document.body.appendChild(m);
     setTimeout(function() {
       var d = 0.5;
       setTimeout(function() { document.body.removeChild(m);}, d * 1000);
-    }, duration);
-}
+    }, duration)
+};
 
 
 function formattime(time){
-    hour = Number(time.substring(0,2))
-    minute = Number(time.substring(3,5))
-    second = Number(time.substring(6,8))
+    hour = Number(time.substring(0,2));
+    minute = Number(time.substring(3,5));
+    second = Number(time.substring(6,8));
     return hour*3600 + minute*60 + second
-}
+};
 
 function sleep(i) {
     return new Promise(function (resolve, reject) {
     setTimeout(function (){resolve()},i);})
-}
+};
 
 
 function getinfo(i) {
@@ -39,7 +39,7 @@ function getinfo(i) {
         jwplayer("div_play").play();
         length = formattime(document.querySelectorAll("td[ng-bind*=\"fcFile.LearnLength|formatSeconds\"]")[i].innerText)
     }
-    catch(err){}
+    catch(err){};
     if (length > 0){
         var s = new XMLHttpRequest();
         s.open("POST", "/G2S/DataProvider/OC/FC2/FCProvider.aspx/FCFileLog_LearnLength_Up", true);
@@ -47,41 +47,41 @@ function getinfo(i) {
         s.send(JSON.stringify({"fcId":fcid,"fcFileId":fcfileid,"learnLength":length}));
         document.getElementsByClassName("ngdialog-close")[0].click();
         Toast('第'+(i+1)+'个进度修改完成',2000);
-        got = true
-    }
-    setTimeout(function (){resolve()},500);
+        got = true;
+    };
+    setTimeout(function (){resolve()},500)
     })
 };
 
 async function get(i){
-    Toast('开始修改第'+(i+1)+'个进度',2000)
+    Toast('开始修改第'+(i+1)+'个进度',2000);
     document.querySelectorAll("a.name.ng-binding")[i].click();
     while (true){
         await getinfo(i);
         await sleep(500);
-        if (got){break;}
+        if (got){break;};
     }
-}
+};
 
 
 async function task(i){
-    length = 0
-    fcfileid = 0
-    fcid = 0
-    got = false
+    length = 0;
+    fcfileid = 0;
+    fcid = 0;
+    got = false;
     await get(i);
     await sleep(200);
-    console.log(length,fcfileid,fcid);
-}
+    console.log(length,fcfileid,fcid)
+};
 
 async function getall() {
     num = document.querySelectorAll("a.name.ng-binding").length;
     getstr = "";
     for (i=0;i<num;i++){
         getstr = getstr + "await task(" + i +");"
-    } 
-    await eval("f = async function() {" + getstr + "};f()")
+    } ;
+    await eval("f = async function() {" + getstr + "};f()");
     Toast('全部进度修改完成',3000)
-}
+};
 
 getall()
